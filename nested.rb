513c16334bd65c5ed9862317be0bea681c913a4b
Charlie Payne <charliepayne1101@gmail.com>
2020-04-13 17:13:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembley"]
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
 return programmer_hash
 end
